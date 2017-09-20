93a97eb79ae3d445fcab3abf426b9b685d3d87b7
rjbrockman <rjbrockman@users.noreply.github.com>
2017-09-20 16:48:16 -0500

    updated array.js with correct version
    When I submitted the original assigment array.js didn't work correctly. It bothered me that it didn't work so
    I got it working now.

--- Week3/array.js
+++ Week3/array.js
@@ -1,15 +1,19 @@
 var main = function () {
     "use strict";
-	function mymax(45, 60, 33, 55){
-    var max = -Infinity, i = 0, n = arr.length;
-
-    for (; i != n; ++i) {
-        if (arr[i] > max) {
-            max = arr[i];
+	var mymax =[45, 60, 33, 55, 85];
+	var max= [] ;
+	max[0] = mymax[0];
+	
+	for (var i = 0; i < mymax.length; i++) {
+		
+		if (max[0] < mymax[i + 1]){
+			max[0] = mymax[i + 1];
+			i++;
+		
         }
-    }
-
-    return max;
-}
+		
+	};
+	console.log(max[0]);
+	return max[0];
 };
 $(document).ready(main);
